e7fa9d98a73f192caf11b1cff7ea14de503ab6f0
MarQ-P <marvinpescos@gmail.com>
2025-09-26 00:17:59 +0800

    added api service for axios

---
+++ frontend/src/shared/services/api.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from "axios";
+import type { AxiosInstance } from "axios";
+import { API_BASE_URL } from "../utils/constant";
+
+const api: AxiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+//request interceptor
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response.data,
+  (error: AxiosError) => {
+    if (error.response?.status == 401) {
+      console.error("Unauthorized, redirecting to login...");
+      window.location.href = "/login"
+    }
+    return Promise.reject(error)
+  }
+);
+
+export default api
---
+++ frontend/src/shared/utils/constant.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const APP_NAME = import.meta.env.VITE_APP_NAME
+export const VITE_FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL

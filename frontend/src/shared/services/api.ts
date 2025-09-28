51917bef951a3aa11454b474e0a057c8538b472e
MarQ-P <marvinpescos@gmail.com>
2025-09-28 09:53:24 +0800

    added persistant localstorage for things like themes, languange, etc.

--- frontend/src/shared/services/api.ts
+++ frontend/src/shared/services/api.ts
@@ -24,10 +24,10 @@ api.interceptors.response.use(
   (error: AxiosError) => {
     if (error.response?.status == 401) {
       console.error("Unauthorized, redirecting to login...");
-      window.location.href = "/login"
+      window.location.href = "/login";
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
 );
 
-export default api
+export default api;
